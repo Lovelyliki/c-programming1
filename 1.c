c2232a4cea22e9158fecdb447fcf261b72bf596c
Lovelyliki <123433792+Lovelyliki@users.noreply.github.com>
2023-01-25 14:07:02 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter an integer");
+	scanf("%d",&num);
+	if (num%2==0)
+	 printf("it is an even number",num);
+	else
+	 printf("it is an odd number",num);
+	return 0;
+	}
